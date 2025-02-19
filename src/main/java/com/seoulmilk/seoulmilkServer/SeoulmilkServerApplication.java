d327ac1faa7736fde66de520f6e29524ee5eaa32
sseongeun <96720963+sseongeun@users.noreply.github.com>
2025-02-19 22:30:54 +0900

    initial commit

---
+++ src/main/java/com/seoulmilk/seoulmilkServer/SeoulmilkServerApplication.java
@@ -0,0 +1,13 @@
+package com.seoulmilk.seoulmilkServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoulmilkServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoulmilkServerApplication.class, args);
+	}
+
+}
