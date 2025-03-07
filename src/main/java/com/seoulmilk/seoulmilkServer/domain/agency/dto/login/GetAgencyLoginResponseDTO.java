b3a1f3158a48e28951e1765ee768f6428435adaf
sseongeun <96720963+sseongeun@users.noreply.github.com>
2025-03-08 00:56:49 +0900

    #41 feat : dto 수정

--- src/main/java/com/seoulmilk/seoulmilkServer/domain/agency/dto/login/GetAgencyLoginResponseDTO.java
+++ src/main/java/com/seoulmilk/seoulmilkServer/domain/agency/dto/login/GetAgencyLoginResponseDTO.java
@@ -16,15 +16,24 @@ public class GetAgencyLoginResponseDTO {
     @Schema(description = "로그인된 대리점 아이디", example = "milksago1234")
     private final String agencyId;
 
+    @Schema(description = "대리점명", example = "서울우유 강동점")
+    private final String agencyName;
+
+    @Schema(description = "대리점 이메일", example = "milksago@gmail.com")
+    private final String email;
+
     @Schema(description = "액세스 토큰")
-     private final String accessToken;
+    private final String accessToken;
 
-     @Schema(description = "리프레시 토큰")
-     private final String refreshToken;
+    @Schema(description = "리프레시 토큰")
+    private final String refreshToken;
 
-    public static GetAgencyLoginResponseDTO of(Agency agency,String accessToken, String refreshToken) {
+    public static GetAgencyLoginResponseDTO of(Agency agency, String accessToken,
+        String refreshToken) {
         return GetAgencyLoginResponseDTO.builder()
             .agencyId(agency.getAgencyId())
+            .agencyName(agency.getAgencyName())
+            .email(agency.getEmail())
             .accessToken(accessToken)
             .refreshToken(refreshToken)
             .build();
