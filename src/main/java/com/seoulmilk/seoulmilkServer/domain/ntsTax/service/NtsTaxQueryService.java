c8240983e6a3343ed723c7f34c12886698e7fa2e
zyovn <yui2507@naver.com>
2025-03-09 00:13:04 +0900

    #57 minor: 불필요한 import문 제거

--- src/main/java/com/seoulmilk/seoulmilkServer/domain/ntsTax/service/NtsTaxQueryService.java
+++ src/main/java/com/seoulmilk/seoulmilkServer/domain/ntsTax/service/NtsTaxQueryService.java
@@ -12,6 +12,6 @@ import java.util.List;
 public interface NtsTaxQueryService {
     Page<NtsTax> getNtsTaxList(Agency agency, Integer page); // 세금 계산서 목록 조회
     Page<NtsTax> searchNtsTaxList(Agency agency, Integer page, LocalDate startDate, LocalDate endDate, List<String> ipNameList); // 세금 계산서 통합 조회 - 조건 기준 탐색
-    // 세금 계산서 진위 여부 검증 후, 상태 및 월별 조회
+    // 세금 계산서 진위 여부 검증 후, 상태 및 월 별 조회
     Page<NtsTax> searchHometaxList(Member member, Integer page, LocalDate startDate, LocalDate endDate, List<String> suNameList, List<String> ipNameList); // 세금 계산서 진위 여부 검증 후, 검색
 }
