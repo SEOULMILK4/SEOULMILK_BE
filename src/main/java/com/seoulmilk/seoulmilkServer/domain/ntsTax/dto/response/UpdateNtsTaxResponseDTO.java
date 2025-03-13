92ea2d7ba3e5f7c5258381aae67a6163b9cdb0fb
zyovn <yui2507@naver.com>
2025-03-13 23:29:49 +0900

    #158 refactor: 다시 -로 통일

--- src/main/java/com/seoulmilk/seoulmilkServer/domain/ntsTax/dto/response/UpdateNtsTaxResponseDTO.java
+++ src/main/java/com/seoulmilk/seoulmilkServer/domain/ntsTax/dto/response/UpdateNtsTaxResponseDTO.java
@@ -11,7 +11,6 @@ import lombok.Getter;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 
 @Getter
 @Builder
@@ -24,8 +23,8 @@ public class UpdateNtsTaxResponseDTO {
     @Schema(description = "승인번호", example = "20240630-06300630-06300201")
     private String issueId;
 
-    @Schema(description = "세금 계산서 작성일자", example = "2025.02.01")
-    private String issueDate;
+    @Schema(description = "세금 계산서 작성일자", example = "2025-02-01")
+    private LocalDate issueDate;
 
     @Schema(description = "공급자 사업등록번호", example = "305-04-02042")
     private String suId;
@@ -64,7 +63,7 @@ public class UpdateNtsTaxResponseDTO {
         return UpdateNtsTaxResponseDTO.builder()
                 .isSuccess(IsSuccess.SUCCESS)
                 .issueId(ntsTax.getIssueId())
-                .issueDate(ntsTax.getIssueDate().format(DateTimeFormatter.ofPattern("yyyy.MM.dd")))
+                .issueDate(ntsTax.getIssueDate())
                 .suId(ntsTax.getSuId())
                 .suName(ntsTax.getSuName())
                 .ipId(ntsTax.getIpId())
